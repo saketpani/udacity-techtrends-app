e24fe263e941f1dd3f95bad9505e97048dad505f
Saket P <saketpani@gmail.com>
2023-08-30 20:42:16 +0530

    updated

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]]],"checksum":"0c2a5eda25c5a2136d234ca83e1a0172ea7a738001da78fbcca3eb35ea684330","vagrant_version":"2.3.7"}
